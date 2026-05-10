891103708829897950cf5f4608b2b610a33a55c7
Ghori kahan <kahan.ghori.cg@gmail.com>
2026-05-10 18:42:16 +0530

    Create 2752-SumMultiples

---
+++ 2752-SumMultiples/2752-SumMultiples.cpp
@@ -0,0 +1,14 @@
+// Last updated: 5/10/2026, 6:42:17 PM
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+};
