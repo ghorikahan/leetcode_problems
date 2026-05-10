290799621f367af3c5877f116499066bbf3cf8d8
Ghori kahan <kahan.ghori.cg@gmail.com>
2026-05-10 18:43:24 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -0,0 +1,18 @@
+// Last updated: 5/10/2026, 6:43:25 PM
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=0;
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] != 0){
+                nums[j] = nums[i];
+                ++j;
+            }
+        }
+
+        for(int i=j;i<nums.size();++i){
+            nums[i] = 0;
+        }
+    }
+};
