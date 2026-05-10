10b315f2a5e21f4c2fc458f8b76ab80a08dee4f6
Ghori kahan <kahan.ghori.cg@gmail.com>
2026-05-10 18:42:10 +0530

    Create 2876-NumberOfEmployeesWhoMetTheTarget

---
+++ 2876-NumberOfEmployeesWhoMetTheTarget/2876-NumberOfEmployeesWhoMetTheTarget.cpp
@@ -0,0 +1,14 @@
+// Last updated: 5/10/2026, 6:42:11 PM
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int count =0;
+        for(int i=0;i<hours.size();i++){
+            if(hours[i] >= target){
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
