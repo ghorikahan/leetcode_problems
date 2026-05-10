ea9bd8b723d092215198a7dff12e5d15dfc3d2d6
Ghori kahan <kahan.ghori.cg@gmail.com>
2026-05-10 18:42:00 +0530

    Create 3446-FindTheNumberOfGoodPairsI

---
+++ 3446-FindTheNumberOfGoodPairsI/3446-FindTheNumberOfGoodPairsI.cpp
@@ -0,0 +1,16 @@
+// Last updated: 5/10/2026, 6:42:01 PM
+class Solution {
+public:
+    int numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int count = 0;
+        for (int i = 0; i < nums1.size(); i++) {
+            for (int j = 0; j < nums2.size(); j++) {
+                if (nums1[i] % (nums2[j] * k) == 0) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
