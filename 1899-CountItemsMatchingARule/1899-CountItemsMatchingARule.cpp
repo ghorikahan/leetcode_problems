d6b2fe0f41a0a278b7027e13c0f3848673aa33eb
Ghori kahan <kahan.ghori.cg@gmail.com>
2026-05-24 09:02:54 +0530

    Create 1899-CountItemsMatchingARule

---
+++ 1899-CountItemsMatchingARule/1899-CountItemsMatchingARule.cpp
@@ -0,0 +1,24 @@
+// Last updated: 5/24/2026, 9:02:55 AM
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey,
+                     string ruleValue) {
+        int count = 0;
+        int index;
+
+        if (ruleKey == "type")
+            index = 0;
+        else if (ruleKey == "color")
+            index = 1;
+        else
+            index = 2;
+
+        for (int i = 0; i < items.size(); i++) {
+            if (items[i][index] == ruleValue) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
