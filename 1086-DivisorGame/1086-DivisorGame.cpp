c27acb6b0447274e6a3a46b24883d028f8b672b7
Ghori kahan <kahan.ghori.cg@gmail.com>
2026-08-05 17:58:45 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.cpp
@@ -0,0 +1,11 @@
+// Last updated: 8/5/2026, 5:58:46 PM
+class Solution {
+public:
+    bool divisorGame(int n) {
+        if(n%2!=0){
+            return false;
+        }
+
+        return true;
+    }
+};
