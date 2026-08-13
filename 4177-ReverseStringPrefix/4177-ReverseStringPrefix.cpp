a8037c11d2d27fcd1e0e50afe5fb81017483d5d7
Ghori kahan <kahan.ghori.cg@gmail.com>
2026-08-13 10:35:26 +0530

    Create 4177-ReverseStringPrefix

---
+++ 4177-ReverseStringPrefix/4177-ReverseStringPrefix.cpp
@@ -0,0 +1,16 @@
+// Last updated: 8/13/2026, 10:35:26 AM
+class Solution {
+public:
+    string reversePrefix(string s, int k) {
+        int left = 0;
+        int right = k - 1;
+
+        while (left < right) {
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+
+        return s;
+    }
+};
