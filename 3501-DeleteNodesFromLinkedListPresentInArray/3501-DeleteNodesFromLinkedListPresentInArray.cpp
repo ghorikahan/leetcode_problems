2d10a3248fd9657257943b5f0277d89afc1abb1e
Ghori kahan <kahan.ghori.cg@gmail.com>
2026-08-13 10:35:46 +0530

    Create 3501-DeleteNodesFromLinkedListPresentInArray

---
+++ 3501-DeleteNodesFromLinkedListPresentInArray/3501-DeleteNodesFromLinkedListPresentInArray.cpp
@@ -0,0 +1,33 @@
+// Last updated: 8/13/2026, 10:35:46 AM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set s1(nums.begin(), nums.end());
+
+        while (head != nullptr && s1.count(head->val)) {
+            head = head->next;
+        }
+        ListNode* temp = head;
+
+        while(temp!=nullptr && temp->next!=nullptr){
+            if(s1.count(temp->next->val)){
+                temp->next = temp->next->next;
+            }
+            else{
+                temp= temp->next;
+            }
+        }
+
+        return head;
+    }
+};
