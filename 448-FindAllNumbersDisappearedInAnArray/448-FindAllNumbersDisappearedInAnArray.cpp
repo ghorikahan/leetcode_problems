5e6f251d3793ce163b949ad2101f1944d38de227
Ghori kahan <kahan.ghori.cg@gmail.com>
2026-08-19 23:45:32 +0530

    Create 448-FindAllNumbersDisappearedInAnArray

---
+++ 448-FindAllNumbersDisappearedInAnArray/448-FindAllNumbersDisappearedInAnArray.cpp
@@ -0,0 +1,16 @@
+// Last updated: 8/19/2026, 11:45:31 PM
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        unordered_set<int> numSet(nums.begin(),nums.end());
+        vector<int> res;
+
+        for(int i=1;i<=nums.size();i++){
+            if(numSet.find(i) == numSet.end()){
+                res.push_back(i);
+            }
+        }
+
+        return res;
+    }
+};
