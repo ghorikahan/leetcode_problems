a4d2889609823e757557419724aba2ccca1f2801
Ghori kahan <kahan.ghori.cg@gmail.com>
2026-09-12 13:07:51 +0530

    Create 709-ToLowerCase

---
+++ 709-ToLowerCase/709-ToLowerCase.cpp
@@ -0,0 +1,18 @@
+// Last updated: 9/12/2026, 1:07:52 PM
+class Solution {
+public:
+    string toLowerCase(string s) {
+        string res = "";
+
+        for(int i=0;i<s.size();i++){
+            if(s[i] >= 'A' && s[i] <= 'Z'){
+                res += s[i]+32;
+            }
+            else{
+                res += s[i];
+            }
+        } 
+
+        return res;
+    }
+};
