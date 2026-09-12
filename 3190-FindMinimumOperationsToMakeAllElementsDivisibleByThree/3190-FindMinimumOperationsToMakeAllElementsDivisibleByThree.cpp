fd541f81c4f6f57e99a0e53087a7e60748c7eba7
Ghori kahan <kahan.ghori.cg@gmail.com>
2026-09-12 13:01:59 +0530

    Create 3190-FindMinimumOperationsToMakeAllElementsDivisibleByThree

---
+++ 3190-FindMinimumOperationsToMakeAllElementsDivisibleByThree/3190-FindMinimumOperationsToMakeAllElementsDivisibleByThree.cpp
@@ -0,0 +1,15 @@
+// Last updated: 9/12/2026, 1:02:00 PM
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int count = 0;
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%3 != 0){
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
