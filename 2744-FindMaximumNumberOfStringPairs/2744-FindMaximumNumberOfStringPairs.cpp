ada64b5687a196758e07edbbc8bbc7627c471330
Ghori kahan <kahan.ghori.cg@gmail.com>
2026-09-12 13:02:53 +0530

    Create 2744-FindMaximumNumberOfStringPairs

---
+++ 2744-FindMaximumNumberOfStringPairs/2744-FindMaximumNumberOfStringPairs.cpp
@@ -0,0 +1,24 @@
+// Last updated: 9/12/2026, 1:02:54 PM
+class Solution {
+public:
+    int maximumNumberOfStringPairs(vector<string>& words) {
+        unordered_map<string,int> mp;
+        int count =0;
+
+        for(int i=0;i<words.size();i++){
+            sort(words[i].begin(),words[i].end());
+
+            mp[words[i]]++;
+        }
+
+        int max = 0;
+
+        for(auto it : mp){
+            if(it.second > 1){
+                max++;
+            }
+        }
+
+        return max;
+    }
+};
