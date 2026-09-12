3f4f60a2ccbe335d29c98e7f4e061fa79293b2eb
Ghori kahan <kahan.ghori.cg@gmail.com>
2026-09-12 13:02:56 +0530

    Create 2703-ReturnLengthOfArgumentsPassed

---
+++ 2703-ReturnLengthOfArgumentsPassed/2703-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,12 @@
+// Last updated: 9/12/2026, 1:02:57 PM
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
