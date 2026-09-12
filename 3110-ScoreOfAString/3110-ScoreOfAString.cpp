e3d90ffbc8c0f998264d1934d707df4f06dc606e
Ghori kahan <kahan.ghori.cg@gmail.com>
2026-09-12 13:02:15 +0530

    Create 3110-ScoreOfAString

---
+++ 3110-ScoreOfAString/3110-ScoreOfAString.cpp
@@ -0,0 +1,20 @@
+// Last updated: 9/12/2026, 1:02:16 PM
+class Solution {
+public:
+    int scoreOfString(string s) {
+        vector<int> num;
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            num.push_back(int(s[i]));
+        }
+
+        for (int j = 0; j < num.size(); j++) {
+            if (j == num.size() - 1) {
+                continue;
+            }
+            res = res + abs(num[j] - num[j + 1]);
+        }
+
+        return res;
+    }
+};
