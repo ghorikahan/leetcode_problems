9e0060fc15285b490b9a6c01d9525116a7ab9b91
Ghori kahan <kahan.ghori.cg@gmail.com>
2026-09-12 13:02:48 +0530

    Create 2723-AddTwoPromises

---
+++ 2723-AddTwoPromises/2723-AddTwoPromises.js
@@ -0,0 +1,17 @@
+// Last updated: 9/12/2026, 1:02:49 PM
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function (promise1, promise2) {
+    let val1 = await promise1;
+    let val2 = await promise2;
+
+    return val1 + val2;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
