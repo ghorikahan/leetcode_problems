a139bae43d61a5cb4f66dbe79b5af39a7fd0ecfc
Ghori kahan <kahan.ghori.cg@gmail.com>
2026-09-12 13:07:04 +0530

    Create 1025-DivisorGame

---
+++ 1025-DivisorGame/1025-DivisorGame.cpp
@@ -0,0 +1,11 @@
+// Last updated: 9/12/2026, 1:07:05 PM
+class Solution {
+public:
+    bool divisorGame(int n) {
+        if(n%2!=0){
+            return false;
+        }
+
+        return true;
+    }
+};
