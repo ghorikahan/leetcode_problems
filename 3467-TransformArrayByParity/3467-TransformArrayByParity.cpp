f53e14d05445f5bf05c0f5bd5aca320e96572989
Ghori kahan <kahan.ghori.cg@gmail.com>
2026-09-12 13:01:35 +0530

    Create 3467-TransformArrayByParity

---
+++ 3467-TransformArrayByParity/3467-TransformArrayByParity.cpp
@@ -0,0 +1,19 @@
+// Last updated: 9/12/2026, 1:01:37 PM
+class Solution {
+public:
+    vector<int> transformArray(vector<int>& nums) {
+        vector<int> res;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] % 2 == 0) {
+                res.push_back(0);
+            } else {
+                res.push_back(1);
+            }
+        }
+
+        sort(res.begin(), res.end());
+
+        return res;
+    }
+};
